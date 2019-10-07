6ace46d86f2a54f63345680e62d185cb19dda41f
joka01002 <joka01002@utb.vaxjo.se>
2019-10-07 08:58:58 +0200

    The whole guess the number code

---
+++ GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random x = new Random();
+            int guess = 0;
+            int num = 0;
+            bool again = true;
+            Console.WriteLine("Welcome to: Guess the number!");
+            Console.WriteLine("In how few guesses can you find the number I'm thinking of?");
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+            while (again)
+            {
+                int y = x.Next(1, 101);
+                while (guess != y)
+                {
+                    guess = int.Parse(Console.ReadLine());
+                    if (guess < y)
+                    {
+                        Console.WriteLine("Your guess is too low, go higher.");
+                        num++;
+                    }
+                    else if (guess > y)
+                    {
+                        Console.WriteLine("Your guess is too high, go lower.");
+                        num++;
+                    }
+                    else if (guess == y)
+                    {
+                        num++;
+                        Console.WriteLine("Yes, you guessed my number, and it only took " + num + " guesses.");
+                    }
+                }
+                Console.WriteLine("Want to play again? yes or no");
+                string ask = Console.ReadLine();
+                if (ask == "no")
+                {
+                    again = false;
+                }
+                else if (ask == "yes")
+                {
+                    guess = 0;
+                    num = 0;
+                }
+            }
+        }
+    }
+}
